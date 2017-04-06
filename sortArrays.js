1ac7dfba11653031814638121a9a38b5d1fd311f
RDMkhoyan <amkhoyan94@mail.ru>
2017-04-06 17:06:59 +0400

    Changes

--- sortArrays.js
+++ sortArrays.js
@@ -1,10 +1,10 @@
 
-/*
+
   var array1 = [5,6,98,22,1];
   var array2 = [2,8,33,1,4,6];
 	for(var  i = 0; i < array2.length; i ++) {
 		array1.push(array2[i]);
-}
+	}
 
 	for(var i = 0; i < array1.length-1; i ++) {
 		for( var j = i+1; j < array1.length; j++ ) {
@@ -18,10 +18,17 @@
 		}
 	}
 
-	alert(array1);*/
-	
+	for( var i = 0; i < array1.length - 1; i ++) {
+		for( var j = i+1; j < array1.length; ++j ) {
+    		while(array1[i] === array1[j]) {
+        		array1.splice(j,1);
+        	}
+		}
+	}
+alert(array1);
 
-  var array1 = [5,6,98,22,1];
+
+  /*var array1 = [5,6,98,22,1];
   var array2 = [2,8,33,1,4,6];
 	for(var  i = 0; i < array2.length; i ++) {
 		array1.push(array2[i]);
@@ -44,4 +51,16 @@
 	}
 
 	alert(array1);
-	
+	*/
+
+
+/*var array = [5,5,5,6,2,8,98,22,8,8,9,9,4,5,1];
+ for( var i = 0; i < array.length - 1; i ++) {
+	for( var j = i+1; j < array.length; ++j ) {
+    	while(array[i] === array[j]) {
+        	array.splice(j,1);
+        	
+        }
+    }
+}
+alert(array);*/
